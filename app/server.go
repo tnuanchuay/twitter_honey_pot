30d860aa27bdd2004997b3d5ae7844cdefaf0d37
Tossapon Nuanchuay <ntossapo@gmail.com>
2020-10-21 01:32:18 +0700

    change port

--- app/server.go
+++ app/server.go
@@ -8,7 +8,7 @@ import (
 func getServer() *http.Server {
 	return &http.Server{
 		Handler:      getRouter(),
-		Addr:         ":8000",
+		Addr:         ":80",
 		WriteTimeout: 1 * time.Second,
 		ReadTimeout:  1 * time.Second,
 	}
